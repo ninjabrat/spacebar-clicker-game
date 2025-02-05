b46250116f49e247a5cfdeb09c4394965671015b
ninjabrat <lameninj@gmail.com>
2025-02-05 10:11:32 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let clickCount = localStorage.getItem('clickCount') ? parseInt(localStorage.getItem('clickCount')) : 0;
+let autoClickerInterval = null;
+const vaultCode = '2025';
 
 document.getElementById('clickCount').innerText = clickCount;
 
@@ -9,3 +11,27 @@ document.body.addEventListener('keydown', function(event) {
         localStorage.setItem('clickCount', clickCount);
     }
 });
+
+function checkVaultCode() {
+    const enteredCode = document.getElementById('vaultCode').value;
+    if (enteredCode === vaultCode) {
+        toggleAutoClicker();
+    } else {
+        alert('Incorrect vault code!');
+    }
+}
+
+function toggleAutoClicker() {
+    if (autoClickerInterval) {
+        clearInterval(autoClickerInterval);
+        autoClickerInterval = null;
+        alert('Auto clicker disabled.');
+    } else {
+        autoClickerInterval = setInterval(() => {
+            clickCount++;
+            document.getElementById('clickCount').innerText = clickCount;
+            localStorage.setItem('clickCount', clickCount);
+        }, 1000);
+        alert('Auto clicker enabled.');
+    }
+}
